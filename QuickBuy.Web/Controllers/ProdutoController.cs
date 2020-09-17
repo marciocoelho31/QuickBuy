6178dc848ef560b2b15b2883c917109679ea1a48
Marcio Coelho <marcio@caster.com.br>
2020-09-17 08:55:58 -0300

    Ajustes na controller de produto e em repositórios

---
+++ QuickBuy.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Contratos;
+using QuickBuy.Dominio.Entidades;
+using System;
+
+namespace QuickBuy.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProdutoController : Controller
+    {
+        private readonly IProdutoRepositorio _produtoRepositorio;
+
+        public ProdutoController(IProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_produtoRepositorio.ObterTodos());        // para chamadas bem sucedidas
+
+                //if (condicao == False)
+                //{
+                //    return BadRequest("mensagem de erro");        // quando for erro
+                //}
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Produto produto)
+        {
+            try
+            {
+                _produtoRepositorio.Adicionar(produto);
+                return Created("api/produto", produto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+    }
+}
