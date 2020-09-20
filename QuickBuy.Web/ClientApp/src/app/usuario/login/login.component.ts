88d6fc2764dc312cffd74122dad9310f79925794
Marcio Coelho <marcio@caster.com.br>
2020-09-20 13:20:40 -0300

    Ajustes para opções de menu de entrar e sair, e acesso com Route Guard

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Usuario } from "../../modelo/usuario";
 
 @Component({
@@ -7,19 +8,27 @@ import { Usuario } from "../../modelo/usuario";
   styleUrls: ["./login.component.css"]
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public usuario;
   //public usuarioAutenticado: boolean;
   //public usuarios = ["usuario1", "usuario2", "usuario3", "usuario4", "usuario5"];
+  public returnUrl: string;
 
-  constructor() {
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {    // iniciando o ciclo de vida do componente
+    this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
     this.usuario = new Usuario();
   }
 
   entrar() {
-    //if (this.usuario.email == "marcio@teste.com" && this.usuario.senha == "teste") {
+    if (this.usuario.email == "marcio@teste.com" && this.usuario.senha == "teste") {
     //  this.usuarioAutenticado = true;
-    //}
+
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate([this.returnUrl]);
+    }
   }
 
 }
