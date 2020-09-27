2fd104bec93fd378c4465385e720887cf81c4cc4
Marcio Coelho <marcio@caster.com.br>
2020-09-27 19:41:29 -0300

    Criando serviços produto e usuario, primeiros testes de autenticação

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Usuario } from "../../modelo/usuario";
+import { UsuarioServico } from "../../servicos/usuario/usuario.servico";
 
 @Component({
   selector: "app-login",
@@ -13,22 +14,49 @@ export class LoginComponent implements OnInit {
   //public usuarioAutenticado: boolean;
   //public usuarios = ["usuario1", "usuario2", "usuario3", "usuario4", "usuario5"];
   public returnUrl: string;
+  public mensagem: string;
 
-  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+  constructor(private router: Router, private activatedRouter: ActivatedRoute,
+    private usuarioServico: UsuarioServico ) {
+    // usar apenas para obter referencia a classes que podem ser injetadas
   }
 
-  ngOnInit(): void {    // iniciando o ciclo de vida do componente
+  ngOnInit(): void {    // iniciando o ciclo de vida do componente // OnInit é chamado DEPOIS do construtor
     this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
     this.usuario = new Usuario();
   }
 
   entrar() {
-    if (this.usuario.email == "marcio@teste.com" && this.usuario.senha == "teste") {
-    //  this.usuarioAutenticado = true;
 
-      sessionStorage.setItem("usuario-autenticado", "1");
-      this.router.navigate([this.returnUrl]);
-    }
+    this.usuarioServico.verificarUsuario(this.usuario)
+      .subscribe(
+        data => {
+          //console.log(data);
+
+          var usuarioRetorno: Usuario;
+          usuarioRetorno = data;
+          sessionStorage.setItem("usuario-autenticado", "1");
+          sessionStorage.setItem("email-usuario", usuarioRetorno.email);
+
+          if (this.returnUrl == null) {
+            this.router.navigate(['/']);
+          } else {
+            this.router.navigate([this.returnUrl]);
+          }
+
+        },
+        err => {
+          console.log(err.error);
+          this.mensagem = err.error;
+        }
+      );
+
+    //if (this.usuario.email == "marcio@teste.com" && this.usuario.senha == "teste") {
+    ////  this.usuarioAutenticado = true;
+
+    //  sessionStorage.setItem("usuario-autenticado", "1");
+    //  this.router.navigate([this.returnUrl]);
+    //}
   }
 
 }
---
+++ QuickBuy.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Entidades;
+using System;
+
+namespace QuickBuy.Web.Controllers
+{
+    [Route("api/[Controller]")]
+    public class UsuarioController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+
+        }
+
+        [HttpPost("VerificarUsuario")]
+        public ActionResult Post([FromBody]Usuario usuario)
+        {
+            try
+            {
+                if (usuario.Email == "marcio@teste.com" && usuario.Senha == "xxx")
+                    return Ok(usuario);
+                
+                return BadRequest("Usuário ou senha inválido");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+
+        }
+
+    }
+}
